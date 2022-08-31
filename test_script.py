0674ed31c57ff4defef7ad01a8bffdc8c125f9ed
Samson Tan <samsontmr@users.noreply.github.com>
2022-08-30 22:27:23 -0700

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,10 +1,14 @@
 import lm_eval
 import json
 import argparse
+import logging
 from accelerate import Accelerator
 
 argparser = argparse.ArgumentParser()
+argparser.add_argument("--model", type=str, choices=["hf-causal", "hf-seq2seq"])
 argparser.add_argument("--branch", type=str, default="add-data-parallel")
+argparser.add_argument("--limit", type=int, default=-1)
+argparser.add_argument("--task_type", type=str, choices=["gen", "cls", "ppl"])
 args = argparser.parse_args()
 
 
@@ -58,26 +62,47 @@ def test_greedy_tasks():
     return tasks
 
 
+if args.limit < 0:
+    limit = None
+else:
+    limit = args.limit
+
+task_map = {'gen': test_greedy_tasks,
+            'ppl': test_loglikehood_rolling,
+            'cls': test_llh_tasks}
+
 if __name__ == "__main__":
     accelerator = Accelerator()
-    # model = lm_eval.get_model("hf-seq2seq", pretrained='google/t5-small-lm-adapt')
-    model = lm_eval.get_model("hf-causal", pretrained="gpt2")
-    tasks = test_greedy_tasks()
-    # tasks = test_llh_tasks()
-    # tasks = test_loglikehood_rolling()
+    if args.model == "hf-seq2seq":
+        pretrained_model = "google/t5-small-lm-adapt"
+    elif args.model == "hf-causal":
+        pretrained_model = "EleutherAI/gpt-neo-1.3B"
+    
+    model = lm_eval.get_model(args.model, pretrained=pretrained_model, user_defined_max_generation_length=50, accelerator=accelerator)
+    tasks = task_map[args.task_type]()
+  
+
 
     if args.branch == "master":
-        results = lm_eval.evaluate(model=model, tasks=tasks)
+        results = lm_eval.evaluate(model=model, tasks=tasks, limit=limit,)
         with open(
-            f"results_batch_size={model.batch_size}-branch={args.branch}.json", "w"
+            f"{args.task_type}_{pretrained_model.split('/')[-1]}_results_batch_size={model.batch_size}-branch={args.branch}.json", "w"
         ) as f:
             json.dump(results, f, indent=2)
     else:
-        for batch_size in [80, 1]:
+        for batch_size in [50, 1]:
+            example_logger = logging.getLogger("examples")
+            filename = f"{args.task_type}_{pretrained_model.split('/')[-1]}_batch_size={batch_size}_examples.jsonl"
+            formatter = logging.Formatter("%(message)s")
+            handler = logging.FileHandler(filename)
+            handler.setFormatter(formatter)
+            example_logger.addHandler(handler)
+            example_logger.setLevel(logging.INFO)
             results = lm_eval.evaluate(
-                model=model, tasks=tasks, batch_size=batch_size, accelerator=accelerator
+                model=model, tasks=tasks, batch_size=batch_size, accelerator=accelerator, limit=limit,
             )
-            with open(
-                f"results_batch_size={batch_size}-branch={args.branch}.json", "w"
-            ) as f:
-                json.dump(results, f, indent=2)
+            if accelerator.is_main_process:
+                with open(
+                    f"{args.task_type}_{pretrained_model.split('/')[-1]}_results_batch_size={batch_size}-branch={args.branch}.json", "w"
+                ) as f:
+                    json.dump(results, f, indent=2)
